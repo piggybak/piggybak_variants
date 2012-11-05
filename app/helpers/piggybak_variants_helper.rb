65811bde213ad093b8bfe172fd3e73922c298a9c
Steph Skardal <steph@endpoint.com>
2012-11-05 11:37:07 -0500

    One more scope fix.

--- app/helpers/piggybak_variants_helper.rb
+++ app/helpers/piggybak_variants_helper.rb
@@ -10,7 +10,7 @@ module PiggybakVariantsHelper
   def variant_map(object)
     map = {}
     object.variants.available.each do |variant|
-      map[variant.option_values.ordered.map { |ov| ov.id }.join('_')] = { :id => variant.piggybak_sellable.id, :price => number_to_currency(variant.piggybak_sellable.price) }
+      map[variant.option_values.hash_ordered.map { |ov| ov.id }.join('_')] = { :id => variant.piggybak_sellable.id, :price => number_to_currency(variant.piggybak_sellable.price) }
     end
     map.to_json
   end
--- app/models/piggybak_variants/option_value.rb
+++ app/models/piggybak_variants/option_value.rb
@@ -6,7 +6,8 @@ module PiggybakVariants
     has_and_belongs_to_many :variants
     belongs_to :option, :inverse_of => :option_values
 
-    scope :ordered, :include => :option, :order => "options.position ASC, option_values.position ASC"
+    scope :ordered, :order => "position ASC"
+    scope :hash_ordered, :include => :option, :order => "options.position ASC, option_values.position ASC"
 
     def admin_label
       "#{self.option.name}: #{self.name}"
