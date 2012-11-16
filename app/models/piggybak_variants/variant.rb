30cd2138efeb5b6130a551fb876c20a0ce52299a
Steph Skardal <steph@endpoint.com>
2012-11-16 11:21:12 -0500

    Fixed migration and attr_accessor updates.

--- app/models/piggybak_variants/variant.rb
+++ app/models/piggybak_variants/variant.rb
@@ -6,6 +6,8 @@ module PiggybakVariants
     belongs_to :item, :polymorphic => true
     has_and_belongs_to_many :option_values
 
+    attr_accessible :option_value_ids, :piggybak_sellable_attributes
+
     scope :available, joins(:piggybak_sellable).where("sellables.active IS TRUE AND (sellables.quantity > 0 OR sellables.unlimited_inventory IS TRUE)") 
     validate :option_value_validation
 
