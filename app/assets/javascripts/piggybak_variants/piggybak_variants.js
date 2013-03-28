679d8d6bcb42e816e9eb432076ef243e74e410d6
Steph Skardal <steph@endpoint.com>
2013-03-28 11:33:45 -0400

    Version bump, see RELEASE_NOTES

--- app/assets/javascripts/piggybak_variants/piggybak_variants.js
+++ app/assets/javascripts/piggybak_variants/piggybak_variants.js
@@ -1,67 +1,66 @@
-function refreshControls(all_selected, selected_key){
-	if(all_selected && variant_map[selected_key]) {
-		$('.variant_options form').show();
-		$('#sellable_id').val(variant_map[selected_key].id);
-		$('#variant_price span').html(variant_map[selected_key].price);
-	} else if(all_selected) { // if all selected and not available
-		$('.variant_options form').hide();
-		$('.unavailable').show();
-	} else { // if not all selected
-		$('.all_selected').show();
-		$('.variant_options form').hide();
-	}    
-};
-
-function extractRadioButtonValues(){
-  var all_selected = true;
-	var selected = new Array();
-	$.each($('.option'), function(i, j) {
-		var option_id = $(j).attr('id');
-		if($('input[name=' + option_id + ']:checked').length) {
-			selected.push($('input[name=' + option_id + ']:checked').val());
-		} else {
-			all_selected = false;	
+var piggybak_variants = {
+	refreshControls : function(data) {
+		if(data.all_selected) { // if all selected
+			var selected_key = data.selected.sort().join('_');
+			if(variant_map[selected_key]) {
+				$('.variant_options form').show();
+				$('#sellable_id').val(variant_map[selected_key].id);
+				$('#variant_price span').html(variant_map[selected_key].price);
+			} else { // if variant not available
+				$('.variant_options form').hide();
+				$('.unavailable').show();
+			}
+		} else { // if not all selected
+			$('.all_selected').show();
+			$('.variant_options form').hide();
 		}
-	});          
-	return {all_selected: all_selected, selected: selected};
-};
-     
-function extractDropdownValues(){     
-  var all_selected = true;
-  $('.variant_options select').each(function() { 
-      if (this.selectedIndex == 0) {
-          all_selected = false;
-      }
-  });
-  var selected = new Array();
-  $.each($('.option'), function(i, j) {
-   var option_id = $(j).attr('id');
-   var select_id = $(j).children("select").attr('id');
-   if($('select#' + select_id + ' option:selected')) {
-     selected.push($('select#' + select_id + ' option:selected').val());
-   } else {
-     all_selected = false; 
-   }
-  }); 
-  return {all_selected: all_selected, selected: selected};
-};  
-
-function toggleVariantForm(){
-  $('.unavailable, .all_selected').hide();
-  var selected_values = {};  
-  if ($('input[type=radio]').length > 0){
-     selected_values = extractRadioButtonValues();
-  }   
-  else{
-      selected_values = extractDropdownValues();
-  };
-	var selected_key = selected_values['selected'].sort().join('_');
-	refreshControls(selected_values['all_selected'], selected_key);
-};
+	},
+	extractRadioButtonValues: function() {
+		var all_selected = true;
+		var selected = new Array();
+		$.each($('.option'), function(i, j) {
+			var option_id = $(j).attr('id');
+			if($('input[name=' + option_id + ']:checked').length) {
+				selected.push($('input[name=' + option_id + ']:checked').val());
+			} else {
+				all_selected = false;	
+			}
+		});					
+		return { all_selected: all_selected, selected: selected };
+	},
+	extractDropdownValues: function() {
+		var all_selected = true;
+		// TODO: Nice to see if easier way to select by value, but select[value=""] doesn't work
+		$('.variant_options select').each(function() {
+			if ($(this).val() == '') {
+				all_selected = false;
+			}
+		});
+		if(!all_selected) {
+			return { all_selected: false, selected: '' };
+		}
+		
+		var selected = new Array();
+		$.each($('.variant_options .option select'), function(i, j) {
+			selected.push($(this).val());
+		});
+		return { all_selected: true, selected: selected};
+	},
+	toggleVariantForm : function() {
+		$('.unavailable, .all_selected').hide();
+		var data;
+		if ($('.variant_options input[type=radio]').length > 0){
+			data = piggybak_variants.extractRadioButtonValues();
+		} else { 
+			data = piggybak_variants.extractDropdownValues();
+		};
+		piggybak_variants.refreshControls(data);
+	}
+}
 
 $(function() {
-	$('.variant_options input[type=radio]').click(function() { toggleVariantForm(); });
-	$('.variant_options select').change(function() { toggleVariantForm(); });
-	toggleVariantForm();
+	$('.variant_options input[type=radio]').click(function() { piggybak_variants.toggleVariantForm(); });
+	$('.variant_options select').change(function() { piggybak_variants.toggleVariantForm(); });
+	piggybak_variants.toggleVariantForm();
 });
 
