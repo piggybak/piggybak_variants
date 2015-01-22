e8ff189d03b993c9e458fbc958fb3475f68b26d7
Maciej Adamczewski <zapalniczek@gmail.com>
2015-01-22 19:02:40 +0100

    Add image to options value

--- app/models/piggybak_variants/option_value.rb
+++ app/models/piggybak_variants/option_value.rb
@@ -1,5 +1,8 @@
 module PiggybakVariants
   class OptionValue < ActiveRecord::Base
+    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    
     has_and_belongs_to_many :variants
     belongs_to :option, :inverse_of => :option_values
 
